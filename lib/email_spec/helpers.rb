d80ba830459e1568d1458075af9d79922098e420
Javier Aranda <javier.aranda.varo@gmail.com>
2012-08-15 21:23:13 +0200

    Solucionado un fallo en un helper para visitar un enlace situado dentro de un correo.

--- lib/email_spec/helpers.rb
+++ lib/email_spec/helpers.rb
@@ -124,8 +124,13 @@ module EmailSpec
     # e.g. Click here in  <a href="http://confirm">Click here</a>
     def parse_email_for_anchor_text_link(email, link_text)
       if textify_images(email.default_part_body) =~ %r{<a[^>]*href=['"]?([^'"]*)['"]?[^>]*?>[^<]*?#{link_text}[^<]*?</a>}
-        URI.split($1)[5..-1].compact!.join("?").gsub("&amp;", "&")
+        #URI.split($1)[5..-1].compact!.join("?").gsub("&amp;", "&")
         # sub correct ampersand after rails switches it (http://dev.rubyonrails.org/ticket/4002)
+        uri = URI($1)
+        uri.scheme = nil
+        uri.host = nil
+        uri.port = nil
+        uri.to_s
       else
         return nil
       end
